645d000da02a1d029a5568fab1ee0d234394aead
yongduek <yongduek.seo@gmail.com>
2017-03-13 21:50:58 +0900

    frozen

--- frozenLake-tf.py
+++ frozenLake-tf.py
@@ -11,6 +11,15 @@ register(
 )
     #reward_threshold=0.78, # optimum = .8196
 
+def eGreedy(env, st, qnetwork, eps=0.1):
+    action = env.action_space.sample()
+    if (qnetwork is not None) and (np.random.uniform() > eps):
+        qs = qnetwork.predict(st)
+        action = np.argmax(qs)
+#
+    return action
+#
+
 def onehot(x):
     '''
     :param x:
@@ -25,17 +34,23 @@ output_size = env.action_space.n
 # make TF network model for approximating Q function
 
 '''
-   Deep Q-learning
-   Q network: input=state/observation, output = Q value of actions
+   Deep SARSA-learning
+   SARSA network: input=state/observation, output = Q value of actions
    for each episode
         initialize
+        observe state
+        select action using policy based on Q
         for t=1, ..., T
-            action = e-greedy {random, max_a Qpred}
             do_action, observe reward and next state
+            selection action2 using policy 
 
+            Q[s,a] <- Q[s,a] + alpha (r + gamma * Q[s',a'] - Q[s,a])
+
+            # do this as update
             y = reward             for terminal state
-                reward + gamma max_a' Q(new_state, a' | theta)
-            perform (y - Q(theta))^2 minimization
+                reward + gamma Q(s', a')
+            perform (y - Q(theta))^2 minimization; this updates Q online. so this is on-policy algorithm
+            #
         endfor
     endfor
 '''
@@ -51,30 +66,35 @@ gamma = 0.99
 episode_count = 1
 for i in range (episode_count):
     state = env.reset() # ob denotes the state of the env
+    action = eGreedy(env, state, qnetwork)
     env.render()
     step_count = 1
     while True:
-        def eGreedy(env, st, qnetwork, eps=0.1):
-            action = env.action_space.sample()
-            if (qnetwork is not None) and (np.random.uniform() > eps):
-                qs = qnetwork.predict(st)
-                action = np.argmax(qs)
-            #
-            return action
-        #
-
-        action = eGreedy(env, state, qnetwork)
-
-        # do action
+        # carry out the action
         stp1, reward, done, _ = env.step (action)
-
         env.render()
+
+        # choose another action from stp1 (for SARSA)
+        action2 =  eGreedy(env, stp1, qnetwork)
 
         print('{}/i{}='.format(step_count, i),
               ' a=', action,
               ' stp1=', stp1,
               ' reward=', reward, ' done=', done)
 
+        targetValue = reward
+        if not done:
+            qvalue = qnetwork.predict (stp1, action2)
+            targetValue += gamma * qvalue
+
+        # the target value of Q(state,action) is this targetValue
+        # so, update Q network with learning rate alpha
+
+        # 
+        state = stp1
+        action = action2
 
         if done:
             break
+#
+print ('finished.')
